a7e4b82484202f3cad95b79532a6177cb792313d
R0uter <lomyr.jo@gmail.com>
2017-04-04 16:06:50 +0800

    fix some ⚠️

--- polyv-ios-client-swift-demo/polyvClasses/SubTitleLabel.h
+++ polyv-ios-client-swift-demo/polyvClasses/SubTitleLabel.h
@@ -5,7 +5,8 @@
 //  Created by seanwong on 2/17/16.
 //  Copyright © 2016 easefun. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
+#import <UIKit/UIView.h>
 #import <Foundation/Foundation.h>
 
 @interface SubTitleLabel : UILabel
